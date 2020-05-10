821c0a58afd7d5e98f20cb24eec77de261b73765
iamvatsal <vatsalkesarwani12@gmail.com>
2020-05-11 03:50:15 +0530

    prog 1 codechef

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
     println("Hello Kotlin")
+    println("This is how to use the main argument, ${args[0]}")
+    add(-1,8)      //function calling
+}
+fun add(a: Int ,b: Int)  //function in kotlin
+{
+    println("Adding $a and $b and we get ${a+b}")
 }
---
+++ src/scan.kt
@@ -0,0 +1,6 @@
+/*Write a program that accepts a number, n, and outputs the same.*/
+
+fun main(args: Array<String>) {
+   val a=readLine()
+   println(a)
+}
